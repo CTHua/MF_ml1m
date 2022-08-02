e5299843e3c3afa142f311a8d6ecc80f7feee7b8
CTHua <illiew2470@gmail.com>
2022-08-02 13:58:29 +0800

    Refactor: Add formula and comment, refactor SVD and biasSVD

--- MF/mf.py
+++ MF/mf.py
@@ -3,9 +3,10 @@ import random
 import numpy as np
 from tqdm import tqdm
 random.seed(0)
+np.random.seed(0)
 
 
-def loadData(dataset_path, split="::"):
+def load_data(dataset_path, split="::"):
     data_item_list = []
     # data views
     # userId::movieId::rating::timestamp (ml-1m)
@@ -17,72 +18,108 @@ def loadData(dataset_path, split="::"):
         temp_tuple[2] = int(temp_tuple[2])  # rating
         temp_tuple[3] = int(temp_tuple[3])  # timestamp
         data_item_list.append(tuple(temp_tuple))
-    # data_item_list = sorted(data_item_list, key=lambda tup: tup[3])
-    # data_item_list = sorted(data_item_list, key=lambda tup: tup[0])
     return data_item_list
 
 
-def getUIMat(data):
+def get_ui_matrix(data):
     # build U-I matrix
     user_list = [i[0] for i in data]
     item_list = [i[1] for i in data]
-    UI_matrix = np.zeros((max(user_list) + 1, max(item_list) + 1))
-    # uimat[u][i] = r
+    ui_matrix = np.zeros((max(user_list) + 1, max(item_list) + 1))
+
     for each_interaction in tqdm(data, total=len(data)):
-        UI_matrix[each_interaction[0]
+        """ ui_matrix[userID][movieID] = rating """
+        ui_matrix[each_interaction[0]
                   ][each_interaction[1]] = each_interaction[2]
-    return UI_matrix
+
+    return ui_matrix
 
 
 class MFModel():
     def __init__(self, R, K, lr=0.0002, beta=0.02, steps=5000):
-        '''
-            R: rating matrix
+        """
+            R: user-item matrix
             K: latent features
             lr: learning rate
             beta: regularization parameter
             steps: iterations
-        '''
+        """
         # data distribution
-        self.userNum, self.movieNum = R.shape
-        self.maxRating = 5
-
-        #
+        self.user_num, self.item_num = R.shape
+        # save params
         self.R = R
         self.K = K
         self.lr = lr
         self.beta = beta
         self.steps = steps
 
-        # result
-        self.recMovie = {}
-
     def train(self):
-        # init
-        self.P = np.random.rand(self.userNum, self.K)
-        self.Q = np.random.rand(self.movieNum, self.K)
-
-        #
-        self.b_u = np.zeros(self.userNum)
-        self.b_i = np.zeros(self.movieNum)
-        self.b = np.mean(self.R[np.where(self.R != 0)])
-
-        #
-        self.samples = [(i, j, self.R[i, j]) for i in range(self.userNum)
-                        for j in range(self.movieNum) if self.R[i, j] > 0]
-
-        # SVD do [steps] round
-        training_process = []
+        # init LFM
+        self.P = np.random.rand(self.user_num, self.K)
+        self.Q = np.random.rand(self.item_num, self.K)
+        # param when use biasSVD
+        self.b_u = np.zeros(self.user_num)
+        self.b_i = np.zeros(self.item_num)
+        self.miu = np.mean(self.R[np.where(self.R != 0)])
+        # 把稀疏矩陣存成陣列
+        self.samples = []
+        for i in range(self.user_num):
+            for j in range(self.item_num):
+                if self.R[i, j] > 0:
+                    self.samples.append((i, j, self.R[i, j]))
+        # do SGD [steps] round
+        history = []
         process = tqdm(range(self.steps), total=self.steps)
         for i in process:
             np.random.shuffle(self.samples)
-            self.biasSVD()
-            mse = self.mse()
-            training_process.append((i, mse))
+            self.SVD()
+            error = self.rmse()
+            history.append((i, error))
             if (i == 0) or ((i+1) % (self.steps / 10) == 0):
-                process.set_description(f"Iteration: {i+1}; error = {mse:.4}")
+                process.set_description(
+                    f"Iteration: {i+1}; error = {error:.4}")
+        return history
 
-        return training_process
+    def SVD(self):
+        """
+            err[u][i] = r[u][i] - r_hat[u][i]
+            r_hat[u][i] = P[u] dot Q[i]
+            P[u][k] = P[u][k] + learning_rate * ( r[u][i] - r_hat[u][i] * Q[i][k] - beta * P[u][k] )
+            Q[i][k] = Q[i][k] + learning_rate * ( r[u][i] - r_hat[u][i] * P[u][k] - beta * Q[i][k] )
+        """
+        for u, i, r in self.samples:
+            r_hat = self.P[u, :].dot(self.Q[i, :].T)
+            e = (r - r_hat)
+            # 透過SGD更新參數
+            self.P[u, :] += self.lr * \
+                (e * self.Q[i, :] - self.beta * self.P[u, :])
+            self.Q[i, :] += self.lr * \
+                (e * self.P[u, :] - self.beta * self.Q[i, :])
+
+    def biasSVD(self):
+        """
+            b_u := b_u + learning_rate * ( r[u][i] - r_hat[u][i] - beta * b_u )
+            b_i := b_i + learning_rate * ( r[u][i] - r_hat[u][i] - beta * b_i )
+            err[u][i] = r[u][i] - r_hat[u][i]
+            r_hat[u][i] = miu(average of R) + b_u + b_i + P[u] dot Q[i]
+            P[u][k] = P[u][k] + learning_rate * ( r[u][i] - r_hat[u][i] * Q[i][k] - beta * P[u][k] )
+            Q[i][k] = Q[i][k] + learning_rate * ( r[u][i] - r_hat[u][i] * P[u][k] - beta * Q[i][k] )
+        """
+        for u, i, r in self.samples:
+            # error
+            r_hat = self.miu + self.b_u[i] + \
+                self.b_i[i]+self.P[u, :].dot(self.Q[i, :].T)
+            e = (r - r_hat)
+            # 透過SGD更新參數
+            self.b_u[u] += self.lr * (e - self.beta * self.b_u[u])
+            self.b_i[i] += self.lr * (e - self.beta * self.b_i[i])
+            self.P[u, :] += self.lr * \
+                (e * self.Q[i, :] - self.beta * self.P[u, :])
+            self.Q[i, :] += self.lr * \
+                (e * self.P[u, :] - self.beta * self.Q[i, :])
+
+    def full_matrix(self):
+        return self.miu + self.b_u[:, np.newaxis] + self.b_i[np.newaxis:, ] + self.P.dot(self.Q.T)
 
     def mse(self):
         xs, ys = self.R.nonzero()
@@ -108,40 +145,13 @@ class MFModel():
             error.append(pow(self.R[x, y] - predicted[x, y], 2))
         return np.sqrt(np.average(error))
 
-    def biasSVD(self):
-        for i, j, r in self.samples:
-            # error
-            prediction = self.get_rating(i, j)
-            e = (r - prediction)
-
-            #
-            """
-
-            """
-            self.b_u[i] += self.lr * (e - self.beta * self.b_u[i])
-            self.b_i[j] += self.lr * (e - self.beta * self.b_i[j])
-
-            # update
-            self.P[i, :] += self.lr * \
-                (e * self.Q[j, :] - self.beta * self.P[i, :])
-            self.Q[j, :] += self.lr * \
-                (e * self.P[i, :] - self.beta * self.Q[j, :])
-
-    def get_rating(self, i, j):
-        prediction = self.b + self.b_u[i] + \
-            self.b_i[j] + self.P[i, :].dot(self.Q[j, :].T)
-        return prediction
-
-    def full_matrix(self):
-        return self.b + self.b_u[:, np.newaxis] + self.b_i[np.newaxis:, ] + self.P.dot(self.Q.T)
-
 
 if __name__ == "__main__":
-    ratingfilePath = os.path.join("ml-1m", "ratings.dat")
-    orgData = loadData(ratingfilePath)
-    uiMatrix = getUIMat(orgData)
+    ratingfile_path = os.path.join("../data/ml-1m", "ratings.dat")
+    origin_data = load_data(ratingfile_path)
+    ui_matrix = get_ui_matrix(origin_data)
 
-    mf = MFModel(uiMatrix, K=2, lr=0.1, beta=0.3, steps=100)
+    mf = MFModel(ui_matrix, K=2, lr=0.1, beta=0.3, steps=100)
     mf.train()
 
     # user_list = [i[0] for i in obs_dataset]
